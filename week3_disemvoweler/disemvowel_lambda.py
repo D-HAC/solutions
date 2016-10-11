871143cd5c1aeb457092cc82805907f252cba13d
Gray <graymury@gmail.com>
2016-10-11 08:16:35 -0400

    Update disemvowel_lambda.py

--- week3_disemvoweler/disemvowel_lambda.py
+++ week3_disemvoweler/disemvowel_lambda.py
@@ -1,3 +1,9 @@
+# Just does basic disemvoweling.
 disemvowel = lambda s: ''.join([(lambda v: '' if v in 'aeiou' else v)(v) for v in list(s)])
 
+# Proper disemvoweling!
+disemvowelWithVowels = lambda s: [''.join([(lambda v: '' if v in 'aeiou' else v)(v) for v in list(s)]), ''.join([(lambda v: v if v in 'aeiou' else '')(v) for v in list(s)])]
+
 # print(disemvowel('Yet another one line solution.'))
+
+[print(s) for s in disemvowelWithVowels('Yet another one line solution. Who said python had to be easy!')]
