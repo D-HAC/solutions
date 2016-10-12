53eb024f43dda6c59ff1b00cd6d7948fb3ac176e
Gray <graymury@gmail.com>
2016-10-12 17:16:34 -0400

    Remove unused dependencies

--- week3_disemvoweler/disemvowel.adb
+++ week3_disemvoweler/disemvowel.adb
@@ -1,6 +1,4 @@
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
-with Ada.Strings.Maps; use Ada.Strings.Maps;
-with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 procedure Disemvoweler is
