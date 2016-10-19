1f7b72d333ed13c3f6459a8dd76aa9da18280049
Gray <graymury@gmail.com>
2016-10-19 10:39:13 -0400

    Update palindromes.c

--- week4_palindromes/palindromes.c
+++ week4_palindromes/palindromes.c
@@ -3,18 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 
-// Compile with GCC on Linux. Not sure if the asm will work on windows
-
 static bool isPalindrome(char* s);
 
-bool isPalindrome(char* s){
-
+bool isPalindrome(char* s) {
+    // Makes search O(n/2+1) instead of O(n)
     int palLen = strlen(s) / 2 + 1;
 
     for (int i = 0; i < palLen; i++) {
 
         char t = s[i]; // Temp
 
+        // Yes, it works.
         asm (
             "cmp %0, %1\n\t" // Compare two ints
             "je true\n\t"    // Jump to true if they're equal
@@ -38,9 +37,5 @@ bool isPalindrome(char* s){
 
 // See if the first command line argument is a palindrome
 void main(int argc, char ** argv) {
-    if (isPalindrome(argv[1])) {
-        printf("True\n");
-    } else {
-        printf("False\n");
-    }
+    printf((isPalindrome(argv[1])) ? "True\n" : "False\n");
 }
